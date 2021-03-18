cf01beb3905c3030411facdbb791bcab7da9bd03
mrprotocoll <43586022+mrprotocoll@users.noreply.github.com>
2021-03-18 17:15:59 +0100

    Update conn.php

--- inc/conn.php
+++ inc/conn.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 class Db{
-    private static $host = "localhost";
-    private static $user = "root";
-    private static $pass = "";
-    private static $db = "crud";
+    private static $host = "us-cdbr-east-03.cleardb.com";
+    private static $user = "b137daf3fe916b";
+    private static $pass = "d4663f22";
+    private static $db = "heroku_05e6751c2b1cef6";
 
     public static function conn(){
         try{
